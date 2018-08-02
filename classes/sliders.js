226db1dc797c8ac01189e5e1023215c1c492ab5c
aboud20-meet <aboud20@meet.mit.edu>
2018-08-02 22:54:05 +0300

    hey!

---
+++ classes/sliders.js
@@ -0,0 +1,2 @@
+$("input[type=range][id=red]").val()
+$("#myLove").css("background-color", $("input[type=range][id=red]").val(), $("input[type=range][id=green]").val(), $("input[type=range][id=blue]").val())
